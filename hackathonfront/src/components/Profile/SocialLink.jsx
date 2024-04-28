aa89a11033493f693cf8b432a21a3546c1be0535
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-04-28 10:20:24 +0300

    add chexbox for user role

--- hackathonfront/src/components/Profile/SocialLink.jsx
+++ hackathonfront/src/components/Profile/SocialLink.jsx
@@ -1,25 +1,34 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { getIcon } from "../icons";
+import { Link } from "react-router-dom";
 
-function SocialLink({ link, onEditLink, onRemoveLink, isNew = false}) {
-  
+function SocialLink({
+  link,
+  onEditLink,
+  onRemoveLink,
+  isNew = false,
+  isLink = false,
+}) {
   const [copyLink, setCopyLink] = useState(link);
   const [isEditing, setIsEditing] = useState(false);
-  
+
   const onChange = useCallback((e) => {
     setCopyLink(e.target.value);
   }, []);
 
-  const onEnter = useCallback((e) => {
-    if (e.key === "Enter" || e.key === "Escape" || e.key === "Tab") {
-      setIsEditing(false);
-      if (copyLink === "") {
-        onRemoveLink();
-      } else {
-        onEditLink(e.target.value);
+  const onEnter = useCallback(
+    (e) => {
+      if (e.key === "Enter" || e.key === "Escape" || e.key === "Tab") {
+        setIsEditing(false);
+        if (copyLink === "") {
+          onRemoveLink();
+        } else {
+          onEditLink(e.target.value);
+        }
       }
-    }
-  }, [copyLink]);
+    },
+    [copyLink]
+  );
 
   const onDoubleClick = useCallback(() => {
     console.log("onDoubleClick");
@@ -38,15 +47,24 @@ function SocialLink({ link, onEditLink, onRemoveLink, isNew = false}) {
       className="list-group-item d-flex justify-content-between align-items-center p-3"
     >
       {getIcon(link)}
-      <input
-        className="mb-0 bg-transparent w-75 text-truncate  border-black"
-        style={{ outline : "0",borderWidth: isEditing ? " 0 0 2px" : "0" }}
-        type="text"
-        disabled={!isEditing}
-        value={copyLink}
-        onChange={onChange}
-        onKeyDown={onEnter}
-      />
+      {isLink && !isEditing ? (
+        <Link
+          to={copyLink}
+          className="text-black footer-links text-decoration-none "
+        >
+          {copyLink}
+        </Link>
+      ) : (
+        <input
+          className="mb-0 bg-transparent w-75 text-truncate  border-black"
+          style={{ outline: "0", borderWidth: isEditing ? " 0 0 2px" : "0" }}
+          type="text"
+          disabled={!isEditing}
+          value={copyLink}
+          onChange={onChange}
+          onKeyDown={onEnter}
+        />
+      )}
     </li>
   );
 }
